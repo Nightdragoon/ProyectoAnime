62391080d01a1da876a33a638e55afa6dab0ffa0
Nightdragoon <crackytrini@gmail.com>
2025-11-22 22:55:48 -0600

    sdfg
    sdfg

--- app.js
+++ app.js
@@ -9,4 +9,16 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 // Middlewares
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hola, este es mi primer servidor Node.js 🚀");
+});
+app.post("/", (req, res) => {
+  res.send("Hola, este es mi primer servidor Node.js 🚀");
+});
+
+// Levantar el servidor
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
+
 export default app;
