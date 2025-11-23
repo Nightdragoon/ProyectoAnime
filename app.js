8e57607f312098fa7a8b6bc344a4edcb9c873bad
Nightdragoon <crackytrini@gmail.com>
2025-11-23 01:53:47 -0600

    adsf
    adsf

--- app.js
+++ app.js
@@ -170,6 +170,8 @@ app.get("/buscarAnime", async (req, res) => {
 
 
 // Levantar el servidor
-app.listen(3000, () => {
-  console.log("Servidor corriendo en http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto " + PORT);
 });
